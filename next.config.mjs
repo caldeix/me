35a4fdf97e5384a2feecda418f56791d58b22566
Caldeiro, Luis M. <caldeiro@protonmail.com>
2025-06-10 22:58:27 +0200

    Corregir configuración de despliegue para GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: '',
+  // Requerido para el despliegue en GitHub Pages en un subdirectorio
+  basePath: '/me',
+  assetPrefix: '/me/',
+
   trailingSlash: true,
   images: {
-    unoptimized: true
-  }
-}
-export default nextConfig
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
