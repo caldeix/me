df62830c7a7f317e88056947102af2d5d617d1e2
Caldeiro, Luis M. <caldeiro@protonmail.com>
2025-06-10 22:50:47 +0200

    config gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/me' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/me/' : '',
   trailingSlash: true,
-  basePath: '/me',
-  assetPrefix: '/me',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true
   }
