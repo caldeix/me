563b75723dc2d4380f0cbb4ffc9bbffb2833ba96
caldeix <caldeiro@protonmail.com>
2026-05-18 23:28:17 +0200

    Updates

---
+++ _next/static/chunks/app/layout-b28e5b0a16e58aaa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7725:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_9860d0"}},9281:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,7725,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[628,441,684,358],()=>s(9281)),_N_E=e.O()}]);
